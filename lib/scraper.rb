772ff92cf799194d389409289896b42601d06c98
taroj khan <tarujkhan@yahoo.com>
2020-03-27 19:14:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ def make_courses
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-      binding.pry
+      
     end
 end 
 
