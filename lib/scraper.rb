0a3c96872d66c8b0ee9c37824d1cf46a5169b195
taroj khan <tarujkhan@yahoo.com>
2020-03-27 19:13:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,18 @@ Scraper.new.get_page
 def get_courses
 self.get_page.css(."post")
 end 
+
+def make_courses
+  
+  doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+      binding.pry
+    end
 end 
+
   # def print_courses
   #   self.make_courses
   #   Course.all.each do |course|
