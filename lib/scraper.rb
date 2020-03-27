9bf737d1beb7662ec6a136c8bbd52da35bbc84fc
taroj khan <tarujkhan@yahoo.com>
2020-03-27 17:35:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,16 +13,16 @@ Scraper.new.get_page
 
 class Scraper
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
   
 end
 
