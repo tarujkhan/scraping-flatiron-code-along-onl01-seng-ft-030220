9e63b474fd959324eeefe782c33f113786b23577
taroj khan <tarujkhan@yahoo.com>
2020-03-27 19:14:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ end
 
 def make_courses
   
-  doc.css(".post").each do |post|
+  self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
